18e5f653129785f1773755875e71fb1f20aa5ef0
Bright Wu <brightwu1995@gmail.com>
2023-09-08 14:44:28 -0700

    implement interactive hamburger menu for smaller screens

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ window.addEventListener("scroll", function() {
 
 })
 
-
 window.addEventListener("scroll", () => {
   const toTop = document.querySelector(".to-top"); 
   if(window.scrollY > 100) {
@@ -13,3 +12,17 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active"); 
   }
 })
+
+/* ------------ Hamburger Menu ------------ */ 
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+function toggleButton() {
+    navList.classList.toggle('show')  
+    menuIcon.classList.toggle('hide'); // Hide the hamburger icon
+    closeIcon.classList.toggle('show'); // Show the close icon
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
